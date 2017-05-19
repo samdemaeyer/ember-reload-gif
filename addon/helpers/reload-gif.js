d7dbee1fd0494feb981921727cc5e354f20fb8bf
Sam De Maeyer <sam@showmyhomework.co.uk>
2017-05-19 14:34:12 +0100

    Add  functionality

--- addon/helpers/reload-gif.js
+++ addon/helpers/reload-gif.js
@@ -2,25 +2,52 @@ import Ember from 'ember';
 
 const { run, String: { htmlSafe } } = Ember;
 
-function reload(id) {
+let reload = (id) => {
   let img = document.querySelector(`#gif-${id}`);
   let origSrc = img.src;
   img.src = '';
   img.src = origSrc;
-}
+};
 
-export function reloadGif(_, attrs) {
-  const random = Math.floor(Math.random() * 100);
+let setDiv = (attrs, random) => {
+  let styles = `background-image: url(${attrs.src}?${random});`;
+  if (attrs.srcset) {
+    let srcSet = attrs.srcset.split(',');
+    srcSet = srcSet.map(element => element.trim());
+    srcSet = srcSet.map(set => {
+      let array = set.split(' ');
+      return `url(${array[0]}?${random}) ${array[1]}`;
+    }).join(', ');
+    styles = `${styles} background-image: -webkit-image-set(${srcSet});`;
+  }
+  return `<div id="gif-${random}" style="${styles}" class="${attrs.class}"></div>`;
+};
+
+let setImg = (attrs, random) => {
   let options = [];
-
   for (let attr in attrs) {
     let option = `${attr}="${attrs[attr]}"`;
     options.push(option);
   }
-
   let attributes = options.join(' ');
+  return `<img id="gif-${random}" ${attributes} />`;
+};
+
+export function reloadGif(_, attrs) {
+  if (!attrs.src && !attrs.srcSet) {
+    console.warn('No "src" or "srcSet" has passed to the `{{reload-gif}}` component');
+  }
+  let htmlString = '';
+  const random = Math.floor(Math.random() * 100);
   run.next(null, reload, random);
-  return htmlSafe(`<img id="gif-${random}" ${attributes} />`);
+
+  if (attrs.backgroundImg) {
+    htmlString = setDiv(attrs, random);
+  } else {
+    htmlString = setImg(attrs, random);
+  }
+
+  return htmlSafe(htmlString);
 }
 
 export default Ember.Helper.helper(reloadGif);
